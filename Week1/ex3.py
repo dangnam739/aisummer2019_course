a21c02eebf1e4bd2179ad140c75cd6192130552d
Kiều Đăng Nam <47108512+ChrisZangNam@users.noreply.github.com>
2019-07-10 22:50:17 +0700

    create function

--- Week1/ex3.py
+++ Week1/ex3.py
@@ -2,3 +2,11 @@
 
 #Cu phap: def <ten_ham> (tham_so):
 #             <noi_dung_ham: thut_le_4_khoang_trong>
+
+def get_groceries():
+    print('milk')
+    print('flour')
+    print('sugar')
+    print('butter')
+
+get_groceries()
