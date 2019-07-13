08c6e9001c4ec7e0c93d6917fb2b61037d1909b4
Kiều Đăng Nam <47108512+ChrisZangNam@users.noreply.github.com>
2019-07-13 23:26:00 +0700

    upddate if_else

--- Week1/ex4.py
+++ Week1/ex4.py
@@ -17,7 +17,7 @@
 #             <code>
 
 #total bill
-total = 2000
+total = 1500
 
 if total >= 1000:
    discout = total * 0.1
@@ -30,7 +30,7 @@ print 'So tien can thanh toan:', total - discout
 
 # function use if_else
 def check_age(age):
-    if (age >= 18):
+    if age >= 18 and age <= 55:
       return True
     else:
         return False
@@ -38,3 +38,21 @@ def check_age(age):
 print(check_age(18))
 print(check_age(16))
 print(check_age(20))
+
+#function if_elif_else
+def discout(total):
+    if total >= 2000:
+       discout = total * 0.2
+       print'Ban duoc giam:', discout
+    elif total >= 1000:
+       discout = total * 0.1
+       print'Ban duoc giam:', discout
+    else:
+       discout = total * 0.05
+       print'Ban duoc giam:', discout
+
+    print 'So tien can thanh toan:', total - discout
+
+discout(1500)
+discout(2500)
+discout(900)
