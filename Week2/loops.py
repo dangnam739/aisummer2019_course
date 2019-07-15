d1bcfda3c85efd7efa0730dd3fbd7e2cbc646e08
Kiều Đăng Nam <47108512+ChrisZangNam@users.noreply.github.com>
2019-07-15 17:30:49 +0700

    loop

--- Week2/loops.py
+++ Week2/loops.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 print("==============While Loop============")
 #                   While Loop
 # syntax:
@@ -44,4 +46,51 @@ for i in d:
     print("%s %d" %(i, d[i]))
 
 
-print("===========Nested Loop===========")  #vong lap long nhau
+print("===========Nested Loop===========")  #v            ong lap long nhau
+
+# for iterator_var in sequence:
+#     for iterator_var in sequence:
+#         statements(s)
+#         statements(s)
+
+# while expression:
+#     while expression:
+#         statement(s)
+#         statement(s)
+
+# example1:
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end=' ')
+    print()
+
+for i in range(2, 10):
+    print(i)
+    print(range(i))
+
+print"======Loop Control Statements========")
+
+# Continue Statement ->It returns the control to the beginning of the loop
+#print all letter except 'a' and 't'
+for letter in 'Python Tutorial':
+    if letter == 'a' or letter == 't':
+        continue
+    print ('Current Letter :', letter)
+    var = 10
+
+print ("===================")
+# Break Statement -> It brings control out of the loop
+
+for letter in 'Python Tutorial':
+    if letter == 'u' or letter == 'l':
+        break
+
+print('Current Letter :', letter)
+
+# Pass Statement  //dung de viet cac vong lap trong
+#We use pass statement to write empty loops.
+#Pass is also used for empty control statement, function and classes.
+
+for letter in 'Python Tutorial':
+  pass
+print("Last Letter :", letter)
