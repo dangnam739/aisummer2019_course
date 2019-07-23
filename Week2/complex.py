50913e502b372c110a5a278cd1e92dc37f39e872
Kiều Đăng Nam <47108512+ChrisZangNam@users.noreply.github.com>
2019-07-23 17:12:02 +0700

    No commit message

---
+++ Week2/complex.py
@@ -0,0 +1,7 @@
+####So phuc
+z = complex(2, 5)  #z = 2+5j
+print(z)
+#print real part
+print(z.real)
+#print imag part
+print(z.imag)
--- Week2/frac.py
+++ Week2/frac.py
@@ -1,1 +1,9 @@
+from __future__ import print_function
 from fractions import *
+
+frac1 = Fraction(10,3)
+frac2 = Fraction(6, 9)
+frac3 = frac1 + frac2
+print(frac1, end=' + ')
+print(frac2, end=' = ')
+print(frac3)
